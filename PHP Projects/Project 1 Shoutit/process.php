5313d0f66a642c4d97dfe941f6e6c93f8d6abfea
Arbab Arain <arbabanjum0347@gmail.com>
2020-11-10 01:27:36 +0500

    Global chat app created

--- PHP Projects/Project 1 Shoutit/process.php
+++ PHP Projects/Project 1 Shoutit/process.php
@@ -4,20 +4,19 @@ if(isset($_POST['sbmt'])){
     $name=$_POST['name'];
     $message=$_POST['message'];
     date_default_timezone_set("Asia/Karachi");
-    $time=date('h:i:sa',time());
+    $time=date('h:i:sa',time()); 
     if($name=='' || !isset($name) || $message=='' || !isset($message)){
         $error = "Please fill in your name and message";
         header("Location: index.php?error=".urlencode($error));
         exit();
     }
     else{
-        $query = 'INSERT INTO `shouts` (`name`, `message`, `time`) 
-        VALUES ('.$name.', '.$message.', '.$time.')';
+        $query = "insert into shouts(`name`, `message`, `time`) values('$name','$message','$time')";
         if($con->query($query)){
-            die("Connection Failed" . mysqli_error($con)); 
+            header("Location: index.php");
         }
         else{
-            header("Location: index.php");
+            echo "Failed to insert";
             exit();
         }
     }
